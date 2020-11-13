b1632eb109af82e3da21d466c306a436c021bd5a
AriiMe <57349348+AriiMe@users.noreply.github.com>
2020-11-13 11:45:32 +0100

    yaaaaaay

--- Day10 big boy/scripts/apiForHomepage.js
+++ Day10 big boy/scripts/apiForHomepage.js
@@ -1,11 +1,11 @@
 window.onload = async () => {
     const url = ("https://striveschool-api.herokuapp.com/api/movies/")
-    let productList = document.querySelector("#sectiontest");
+    let productList = document.querySelector("#section1");
     let urlParams = new URLSearchParams(window.location.search);
     id = urlParams.get("id");
     
     try {
-      let response = await fetch(url + "test", {
+      let response = await fetch(url + "test5", {
         headers: new Headers({
           Authorization:
             "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZmFiYzRjOTRiY2RlMTAwMTc2MTZhODAiLCJpYXQiOjE2MDUxMDg4MDEsImV4cCI6MTYwNjMxODQwMX0.mtbbZ989-aU75_cKH3KnULhH7anOrPQdcdW1R7MQTSk",
